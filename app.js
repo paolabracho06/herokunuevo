e3a0b0042e456bc3a18e3936b251acdd3f5c797e
paolabracho06 <paolabracho683@gmail.com>
2021-11-24 23:08:43 -0300

    Paola Fernandez

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.listen(process.env.PORT || 3000, ()=> (console.log('Servidor funcionando'));
